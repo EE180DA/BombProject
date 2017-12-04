1fd410b0343debb3a052bd342816bb2d5d70711d
Jiale <jackhe@ucla.edu>
2017-12-04 01:12:30 -0800

    Added functionalities to main function. Improved Image Recognition. Added video

--- ImageRecognition/detectShapes.py
+++ ImageRecognition/detectShapes.py
@@ -4,55 +4,82 @@ import imutils
 import cv2
 import numpy as np 
 
-cap = cv2.VideoCapture(0)
 
-while(True):
-	if not cap.isOpened():
-		cap.open()
-	ret, frame = cap.read()
-	if ret:
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		resized = imutils.resize(frame, width=300)
-		ratio = frame.shape[0] / float(resized.shape[0])
-		gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-		blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-		thresh = cv2.threshold(blurred, 120, 255, cv2.THRESH_BINARY)[1]
-		thresh = cv2.bitwise_not(thresh)
-		# kernel = np.ones((5,5),np.uint8)
-		# dilation = cv2.dilate(thresh,kernel,iterations = 1)
-		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-		sd = ShapeDetector()
-		cd = ColorDetector()
-		
-		for c in cnts:
-			# compute the center of the contour, then detect the name of the
-			# shape using only the contour
-			M = cv2.moments(c)
-			cX = 0
-			cY = 0
-			if (M["m00"] != 0):
-				cX = int((M["m10"] / M["m00"]))
-				cY = int((M["m01"] / M["m00"]))
+class DetectShapes:
 
-			shape = sd.detect(c)
-	 		# color = cd.detect(frame[cX, cY])
-	 		color = "unknown"
-	 		if cX < resized.shape[0]-20 and cX > 20 and cY < resized.shape[1]-20 and cY > 20:
-	 			pixel = resized[cX-5:cX+5, cY-5:cY+5]
-	 			color = cd.detect(pixel)
-			# multiply the contour (x, y)-coordinates by the resize ratio,
-			# then draw the contours and the name of the shape on the image
-			c = c.astype("float")
-			# c *= ratio
-			c = c.astype("int")
-			cv2.drawContours(resized, [c], -1, (0, 255, 0), 2)
-			cv2.putText(resized, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-	 		cv2.putText(resized, color, (cX, cY-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-			# show the output image
+	def __init__ (self):
+		pass
 
-		cv2.imshow('frame', resized)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-cap.release()
-cv2.destroyAllWindows()
+	def start_minigame(self):
+		cap = cv2.VideoCapture(0)
+		detectCount = 0
+		while(True):
+			if not cap.isOpened():
+				cap.open()
+			ret, frame = cap.read()
+			fullyDetected = False
+			if ret:
+				hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+				resized = imutils.resize(frame, width=300)
+				ratio = frame.shape[0] / float(resized.shape[0])
+				gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+				blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+				thresh = cv2.threshold(blurred, 120, 255, cv2.THRESH_BINARY)[1]
+				thresh = cv2.bitwise_not(thresh)
+				# kernel = np.ones((5,5),np.uint8)
+				# dilation = cv2.dilate(thresh,kernel,iterations = 1)
+				cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+				cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+				sd = ShapeDetector()
+				cd = ColorDetector()
+				detected = False
+				for c in cnts:
+					# compute the center of the contour, then detect the name of the
+					# shape using only the contour
+					M = cv2.moments(c)
+					cX = 0
+					cY = 0
+					if (M["m00"] != 0):
+						cX = int((M["m10"] / M["m00"]))
+						cY = int((M["m01"] / M["m00"]))
+
+					shape = sd.detect(c)
+			 		# color = cd.detect(frame[cX, cY])
+			 		color = "unknown"
+			 		if cX < resized.shape[0]-20 and cX > 20 and cY < resized.shape[1]-20 and cY > 20:
+			 			pixel = resized[cX-5:cX+5, cY-5:cY+5]
+			 			color = cd.detect(pixel)
+					# multiply the contour (x, y)-coordinates by the resize ratio,
+					# then draw the contours and the name of the shape on the image
+					c = c.astype("float")
+					# c *= ratio
+					c = c.astype("int")
+					cv2.drawContours(resized, [c], -1, (0, 255, 0), 2)
+					cv2.putText(resized, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+			 		cv2.putText(resized, color, (cX, cY-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+					# show the output image
+					#print shape + ' ' + color
+					if shape == "triangle" and color == "black":
+						detected = True
+				if detected:
+					detectCount += 1
+				else:
+					detectCount = 0
+				#print detectCount
+				if detectCount > 9:
+					fullyDetected = True
+				
+
+				cv2.imshow('frame', resized)
+				if fullyDetected:
+					break
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
+		cap.release()
+		cv2.destroyAllWindows()
+		if detected:
+			return 1
+
+if __name__ == '__main__':
+	d = DetectShapes()
+	d.start_minigame()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import time
 from random import shuffle
-
+import threading
+import thread
+from ImageRecognition.detectShapes import DetectShapes
+import sys
+import os
+import webbrowser
 class GameInstance:
 
 	def __init__(self, diff = -1):
@@ -8,13 +13,15 @@ class GameInstance:
 			diff = raw_input("Please choose a difficulty level (1-3): ")
 			diff = int(diff)
 		self.difficulty = diff
-		self.time = 50*self.difficulty #time left in seconds
+		self.time = 240/self.difficulty #time left in seconds
 		self.timeleft = self.time+3
 		
 		self.complete = False
-		self.minigames = ['wires', 'images', 'gestures', 'voice']
+		self.minigames = ['buttons', 'images', 'gestures', 'voice']
 		shuffle(self.minigames)
 		self.minigames.append('wirecutting')
+		self.thread = threading.Thread(target = self.timer, args = ())
+		self.thread.daemon = True
 
 
 	def kill(self):
@@ -25,55 +32,71 @@ class GameInstance:
 		return self.difficulty
 
 	def get_time(self):
-		print "Time left: " + str(int(self.timeleft))
+		print "\nTime left: " + str(int(self.timeleft))
 		return int(self.timeleft)
 
-	def print_timeleft(self, startTime, oldtimeleft):
-		timeDiff = int(time.time()) - startTime
-		self.timeleft = self.time - timeDiff
-		if oldtimeleft-self.timeleft >= 1:
-			self.get_time()
-			return self.timeleft
-		return oldtimeleft
-			#Also write new time to LCD screen
-
-	def start_game(self):
+	def timer(self):
 		startTime = int(time.time())
 		oldtimeleft = self.timeleft
+		while True:
+			timeDiff = int(time.time()) - startTime
+			self.timeleft = self.time - timeDiff
+			self.get_time()
+			time.sleep(1)
+			if(self.timeleft <= 0):
+				print "BOOM!"
+				webbrowser.open("https://www.youtube.com/watch?v=wdXU4R8JBe4", new=0, autoraise=True)
+				thread.interrupt_main()
+				sys.exit(0)
+
+
+	def intro(self):
+		print "Welcome to the bomb defusal training module!"
+		time.sleep(3)
+		print "You have chose the difficulty level: " + str(self.difficulty)
+		time.sleep(1)
+		print "That means you have exactly " + str(self.time) + "s to defuse this bomb!"
+		time.sleep(2)
+		print "I hope you are ready because it's about to get intense in here!"
+		time.sleep(3)
+		print "3"
+		time.sleep(1)
+		print "2"
+		time.sleep(1)
+		print "1"
+		time.sleep(1)
+		print "0"
+
+	def start_game(self):
+		self.intro()
 		currGame = 1
+		self.thread.start()
 		while(not self.complete and self.timeleft > 0):
-			oldtimeleft = self.print_timeleft(startTime, oldtimeleft)
 				#Also write new time to LCD screen
 
-			print "Time to play: " + self.minigames[0]
-			while(self.timeleft > 0):
-				oldtimeleft = self.print_timeleft(startTime, oldtimeleft)
-				if(self.timeleft < self.time-5):
-					break
+			print "\nTime to play: " + self.minigames[0]
+			result = self.start_minigame(self.minigames[0])
+			if result == 1:
+				print "Congratulations you passed the first level"
+			print "\nTime to play: " + self.minigames[1]
+			result = self.start_minigame(self.minigames[1])
+			if result == 1:
+				print "Congratulations you passed the second level"	
 
-			print "Time to play: " + self.minigames[1]
-			while(self.timeleft > 0):
-				oldtimeleft = self.print_timeleft(startTime, oldtimeleft)
-				if(self.timeleft < self.time-10):
-					break
+			print "\nTime to play: " + self.minigames[2]
+			result = self.start_minigame(self.minigames[2])
+			if result == 1:
+				print "Congratulations you passed the third level"
 
-			print "Time to play: " + self.minigames[2]
-			while(self.timeleft > 0):
-				oldtimeleft = self.print_timeleft(startTime, oldtimeleft)
-				if(self.timeleft < self.time-15):
-					break
+			print "\nTime to play: " + self.minigames[3]
+			result = self.start_minigame(self.minigames[3])
+			if result == 1:
+				print "Congratulations you passed the fourth level"
 
-			print "Time to play: " + self.minigames[3]
-			while(self.timeleft > 0):
-				oldtimeleft = self.print_timeleft(startTime, oldtimeleft)
-				if(self.timeleft < self.time-20):
-					break
-
-			print "Time to play: " + self.minigames[4]
-			while(self.timeleft > 0):
-				oldtimeleft = self.print_timeleft(startTime, oldtimeleft)
-				if(self.timeleft < self.time-25):
-					break
+			print "\nTime to play: " + self.minigames[4]
+			result = self.start_minigame(self.minigames[4])
+			if result == 1:
+				print "Congratulations you passed the last level"
 			self.complete = True
 
 
@@ -86,7 +109,24 @@ class GameInstance:
 		print '[%s]'%', '.join(map(str, self.minigames))
 		return self.minigames
 
-
+	def start_minigame(self, game_name):
+		if game_name == "images":
+			print "Draw a black triangle!"
+			d = DetectShapes()
+			result = d.start_minigame()
+		elif game_name == "buttons":
+			result = 1
+			time.sleep(5)
+		elif game_name == "gestures":	
+			result = 1
+			time.sleep(5)
+		elif game_name == "voice":	
+			result = 1
+			time.sleep(5)
+		elif game_name == "wirecutting":
+			result = 1
+			time.sleep(5)
+		return result	
 
 
 if __name__ == '__main__':
