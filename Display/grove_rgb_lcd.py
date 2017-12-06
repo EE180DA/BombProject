b0c451ab561ff1861b8bd41a446f865b6b38fea9
JaeWonJo <jwjo111@gmail.com>
2017-12-05 16:18:46 -0800

    Display in python

--- Display/grove_rgb_lcd.py
+++ Display/grove_rgb_lcd.py
@@ -40,17 +40,12 @@ THE SOFTWARE.
 
 import time,sys
 
-if sys.platform == 'uwp':
-    import winrt_smbus as smbus
-    bus = smbus.SMBus(1)
-else:
-    import smbus
-    import RPi.GPIO as GPIO
-    rev = GPIO.RPI_REVISION
-    if rev == 2 or rev == 3:
-        bus = smbus.SMBus(1)
-    else:
-        bus = smbus.SMBus(0)
+import mraa
+
+busrgb=mraa.I2c(0)
+busrgb.address(0x62)
+buslcd=mraa.I2c(1)
+buslcd.address(0x3e)
 
 # this device has two I2C addresses
 DISPLAY_RGB_ADDR = 0x62
@@ -58,37 +53,37 @@ DISPLAY_TEXT_ADDR = 0x3e
 
 # set backlight to (R,G,B) (values from 0..255 for each)
 def setRGB(r,g,b):
-    bus.write_byte_data(DISPLAY_RGB_ADDR,0,0)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,1,0)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,0x08,0xaa)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,4,r)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,3,g)
-    bus.write_byte_data(DISPLAY_RGB_ADDR,2,b)
+    busrgb.writeReg(0,0)
+    busrgb.writeReg(1,0)
+    busrgb.writeReg(0x08,0xaa)
+    busrgb.writeReg(4,r)
+    busrgb.writeReg(3,g)
+    busrgb.writeReg(2,b)
 
 # send command to display (no need for external use)    
 def textCommand(cmd):
-    bus.write_byte_data(DISPLAY_TEXT_ADDR,0x80,cmd)
+    buslcd.writeReg(0x80,cmd)
 
 # set display text \n for second line(or auto wrap)     
-def setText(text):
-    textCommand(0x01) # clear display
+def setText(text,rows):
+    textCommand(0x02) # clear display
     time.sleep(.05)
     textCommand(0x08 | 0x04) # display on, no cursor
     textCommand(0x28) # 2 lines
     time.sleep(.05)
-    count = 0
-    row = 0
+    erase = "                "
+    if rows == 2:
+        textCommand(0xc0)
+        for c in erase:
+            buslcd.writeReg(0x40,ord(c))
+        textCommand(0xc0)
+    if rows == 1:
+        textCommand(0x80)
+        for c in erase:
+            buslcd.writeReg(0x40,ord(c))
+        textCommand(0x80)
     for c in text:
-        if c == '\n' or count == 16:
-            count = 0
-            row += 1
-            if row == 2:
-                break
-            textCommand(0xc0)
-            if c == '\n':
-                continue
-        count += 1
-        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+        buslcd.writeReg(0x40,ord(c))
 
 #Update the display without erasing the display
 def setText_norefresh(text):
@@ -111,7 +106,7 @@ def setText_norefresh(text):
             if c == '\n':
                 continue
         count += 1
-        bus.write_byte_data(DISPLAY_TEXT_ADDR,0x40,ord(c))
+        buslcd.writeReg(0x40,ord(c))
 
 # example code
 if __name__=="__main__":
