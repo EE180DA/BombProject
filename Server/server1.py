92a1ba6f441cfcf847a9d6bd5a338b49432eb8b8
Bryan-Mau <bryanm1010@gmail.com>
2017-12-12 12:11:46 -0800

    Changed server implementation

--- Server/server1.py
+++ Server/server1.py
@@ -1,27 +1,39 @@
-mport socket
+import socket
 import threading
 
-bind_ip = '0.0.0.0'
-bind_port = 9999
+class Server1:
+    #parameters for the server
+    def __init__(self):
+        self.bind_ip = '192.168.42.1'
+        self.bind_port = 3999
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((bind_ip, bind_port))
-server.listen(5)  # max backlog of connections
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.bind_ip, self.bind_port))
+        self.server.listen(5)  # max backlog of connections
+        print 'Listening on {}:{}'.format(self.bind_ip, self.bind_port)
 
-print 'Listening on {}:{}'.format(bind_ip, bind_port)
-
-
-def handle_client_connection(client_socket):
+    #Sends a message to the client to indicate the start of a game, then received the result of that game    
+    def handle_client_connection(self, client_socket, game_code):
+        client_socket.send(game_code)
         request = client_socket.recv(1024)
-        print 'Received {}'.format(request)
-                client_socket.send('ACK!')
-                    client_socket.close()
+        if request == "Fail":
+            client_socket.close() 
+            return(0)
+        if request == "Success":
+            client_socket.close() 
+            return(1)
+            
+    #Starts the server, waits for a connection with a client, receives the result of the game(Fail or Success)
+    def start_server(self, game_code):
+        while True:
+            client_sock, address = self.server.accept()
+            print 'Accepted connection from {}:{}'.format(address[0], address[1])
+            result = self.handle_client_connection(client_sock, game_code)                              
+            return result
+            
 
-                    while True:
-                            client_sock, address = server.accept()
-                                print 'Accepted connection from {}:{}'.format(address[0], address[1])
-                                    client_handler = threading.Thread(
-                                                    target=handle_client_connection,
-                                                            args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
-                                                                )
-                                        client_handler.start()i
+if __name__ == '__main__':
+    g = Server()
+    code = "1234"
+    c = g.start_server(code)
+    print c
