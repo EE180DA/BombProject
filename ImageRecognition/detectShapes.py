8a6f14e0eb27fb7bc8d0665c380141353efa180f
Jackhe96 <jackhe219@gmail.com>
2017-12-12 00:03:02 +0000

    Added stuff

--- ImageRecognition/detectShapes.py
+++ ImageRecognition/detectShapes.py
@@ -27,7 +27,7 @@ class DetectShapes:
 			fullyDetected = False
 			if ret:
 				resized = imutils.resize(frame, width=300)
-				ycb = cv2.cvtColor(resized, cv2.COLOR_BGR2YCrCb)
+				ycb = cv2.cvtColor(resized, cv2.COLOR_BGR2YCR_CB)
 				ratio = frame.shape[0] / float(resized.shape[0])
 				gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 				blurred = cv2.GaussianBlur(gray, (5, 5), 0)
