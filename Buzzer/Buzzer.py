c812bc7497d6b2b41b6930be8b214191e483f744
Jiale He <jackhe@g.ucla.edu>
2018-02-11 16:32:44 -0800

    Added buzzer code

--- Buzzer/Buzzer.py
+++ Buzzer/Buzzer.py
@@ -1,30 +1,30 @@
 import time
 from upm import pyupm_buzzer as upmBuzzer
 
-def main():
-    # Create the buzzer object using GPIO pin 5
-    class Buzzer():
-        def __init__ (self):
-        pass
 
 
-    buzzer = upmBuzzer.Buzzer(5)
+class Buzzer():
+    def __init__ (self, pin_number):
+        buzzer = upmBuzzer.Buzzer(pin_number)
 
-    chords = [1000];
+
+    sounds = {"right" : [1000, 250, 500], "wrong" : [5000, 6000], "win" : [500, 4000, 3000, 300], "lose" : [1000]}  
+    durations = {"right" : [50000, 50000, 50000], "wrong" : [10000, 500000], "win" : [200000, 50000, 30000, 100000], "lose" : [800000]}
+    delay = {"right" : 0.01, "wrong" : 0.1, "win" : 0.01, "lose" : 0.1}
+
+    def play(self, name):
+        if name in sounds:
+            sound = sounds[name]
+            duration = durations[name]
+            for i in range (0, len(sound)):
+                print(buzzer.playSound(sound[i]), delay[i])
+                time.sleep(delay[name])
 
     # Print sensor name
     print(buzzer.name())
 
     # Play sound (DO, RE, MI, etc.), pausing for 0.1 seconds between notes
-    for chord_ind in range (0,7):
-        # play each note for a half second
-        print(buzzer.playSound(chords[chord_ind], 500000))
-        time.sleep(0.1)
-
-    print("exiting application")
-
-    # Delete the buzzer object
-    del buzzer
 
 if __name__ == '__main__':
-    main()
+    buzzer = Buzzer(5)
+    buzzer.play(right)
